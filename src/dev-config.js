4dc53b5e65f5021bf4be8ca74a8e31a66ea6f37f
MarcWong <yaowang95@pku.edu.cn>
2020-05-22 15:19:38 +0800

    fix

--- src/dev-config.js
+++ src/dev-config.js
@@ -3,11 +3,10 @@ const Bucket = 'offpay-dev-1256235662'
 const Region = 'ap-guangzhou'
 const isDev = true
 
-
 export default {
   name: 'Config',
   baseUrl,
   Bucket,
   Region,
-  isDev,
+  isDev
 }
